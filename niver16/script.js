0acf6daf8015af1b92b7d810063c11748eb76418
ViniciusGregorio <112400890+ViniciusGregorio@users.noreply.github.com>
2023-10-13 23:22:26 -0300

    Add files via upload

---
+++ niver16/script.js
@@ -0,0 +1,7 @@
+document.getElementById("surpresaButton").addEventListener("click", function() {
+    alert("Giovanna \nNeste dia você completa mais um ano de vida !! E olha só... 16 anos.\nFico muito feliz e orgulhoso em ver quem você tem se tornado, como Deus tem transformado e aperfeiçoado seu caráter.Uma mulher de Deus, inteligente, dedicada e sábia;e ainda muito mais. \nTenho certeza que você ainda vai muito longe e que grandes coisas tem o Senhor guardado para ti nessa sua fase;não posso desejar menos que tudo de bom, e a provisão do Senhor para você.\nVou te ver ainda casar, se formar e pregar e ministrar a muitas vidas, que em todas essas conquistas e proezas você nunca se esqueça de onde veio e, principalmente, para Onde vai: o lugar de eterna glória e paz ao lado de Jesus.\nTenha um ótimo dia e nunca esqueça que eu te amo e torço por você!! E mais do que eu ou qualquer pessoa, o Senhor te viu e te chamou !!!!\n \n²⁴ O Senhor te abençoe e te guarde;\n²⁵ O Senhor faça resplandecer o seu rosto sobre ti, e tenha misericórdia de ti;\n²⁶ O Senhor sobre ti levante o seu rosto e te dê a paz. (Números 6:24-26)\n \n¹⁷ Assim que, se alguém está em Cristo, nova criatura é; as coisas velhas já passaram; eis que tudo se fez novo  (2 Coríntios 5:17)"
+
+    );
+   
+   
+});
